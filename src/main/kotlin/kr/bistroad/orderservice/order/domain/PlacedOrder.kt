2966c51667bb8b29bffaba2f2ead41b51c0c920b
elsiff <elsiff1002@gmail.com>
2020-10-05 15:44:14 +0900

    Fix equality issue of PlacedOrder

--- src/main/kotlin/kr/bistroad/orderservice/order/domain/PlacedOrder.kt
+++ src/main/kotlin/kr/bistroad/orderservice/order/domain/PlacedOrder.kt
@@ -20,4 +20,19 @@ data class PlacedOrder(
 ) {
     val hasReview: Boolean
         get() = reviews.isNotEmpty()
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as PlacedOrder
+
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
 }
